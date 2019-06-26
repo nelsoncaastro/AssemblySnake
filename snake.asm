241d08741547258ac307fe6961ff92416fdcee75
Unknown <00043516@uca.edu.sv>
2019-06-26 03:54:24 -0600

    Going to the design state, basic structure done

--- snake.asm
+++ snake.asm
@@ -1,6 +1,8 @@
-org 100h ;Indica en que dirección de memoria comienza el programa
+org 100h ;Indica en que dirección de memoria comienza el programa.
 
-section .bss ;Sección donde declaramos variables, solo reservamos memoria
+section .bss ;Sección donde declaramos variables, solo reservamos memoria.
+
+section .data ;Sección donde inicializamos variables.
 
 section .text ;Sección del código fuente
 
@@ -18,4 +20,33 @@ iniciarModoVideo:
 	int 10h
 	ret
 
+clear_screen:
+	mov ah, 06h ; Función scroll up
+	mov al, 00h ; Clear
+	mov bh, 00h ; Atributo
+	mov cl, 00h ; Columna inicial
+	mov ch, 00h ; Fila inicial
+	mov dl, 4fh ; Columna final 
+	mov dh, 1dh ; Fila final
+	int 10h
+	ret
 
+drawSnake:
+
+	ret
+
+pixelBlanco:
+	mov ah, 0Ch
+	mov al, 1111b ;blanco
+	mov bh, 00h
+	int 10h
+	ret
+
+espera:
+	mov ah, 00
+	int 16h
+	ret
+	
+fin: 
+	int 21h
+	ret
